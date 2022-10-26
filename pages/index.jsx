e5d81194de333aff0d509c91135dac0569ffcb1a
king <miabiyewisdom@gmail.com>
2022-10-26 12:22:51 -0700

    loader, navbar implemented

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import toast, {Toaster} from 'react-hot-toast'
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <div>
-     
+     <Loader show />
+     <button onClick={() => toast.success('Toast me!')}>Toast Me!</button>
     </div>
   )
 }
